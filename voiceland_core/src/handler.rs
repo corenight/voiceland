ebbe1918d4a76a1778759d038fe6385e4d463481
Gátomo <63877602+gatomod@users.noreply.github.com>
2023-10-16 14:21:30 +0200

    fix: keychain code

--- voiceland_core/src/handler.rs
+++ voiceland_core/src/handler.rs
@@ -1,6 +1,8 @@
+use core::panic;
+
 use anyhow::{bail, Result};
 use futures::stream::FuturesUnordered;
-use quinn::{Connecting, ConnectionError};
+use quinn::{Connecting, ConnectionError, RecvStream, SendStream};
 use tokio::sync::broadcast::Sender;
 use voiceland_common::logs;
 
@@ -56,17 +58,70 @@ pub async fn handler(conn: Connecting, tx: Sender<Vec<u8>>) -> Result<()> {
 
     let conn = conn.await?;
 
+    loop {
+        let (mut send, mut recv) = match conn.accept_bi().await {
+            Err(ConnectionError::ApplicationClosed { .. })
+            | Err(ConnectionError::ConnectionClosed { .. })
+            | Err(ConnectionError::TimedOut { .. }) => break,
+            Err(err) => bail!(err),
+            Ok(a) => a,
+        };
+
+        let tx1 = tx.clone();
+        let tx2 = tx.clone();
+        tokio::spawn(async move {
+            if let Err(err) = broadcast(tx1, &mut send).await {
+                panic!(
+                    "El tokio::sync::broadcast se queda más colgado que un llavero\n{}",
+                    err
+                )
+            }
+        });
+
+        tokio::spawn(async move {
+            if let Err(err) = recv_packet(tx2, &mut recv).await {
+                panic!(
+                    "El quinn::RecvStream se queda más colgado que un llavero\n{}",
+                    err
+                )
+            }
+        });
+    }
+
+    Ok(())
+}
+
+async fn broadcast(tx: Sender<Vec<u8>>, send: &mut SendStream) -> Result<()> {
     let mut rx = tx.subscribe();
 
-    tx.send(b"larva".to_vec())?;
+    let msg = rx.recv().await;
 
-    tokio::select! {
-        msg = rx.recv() => {
-            if let Ok(msg) = msg {
-                println!("[ MPMC ] {}", String::from_utf8_lossy(&msg));
-            }
-        }
+    if let Ok(msg) = msg {
+        println!("[ MPMC ] {}", String::from_utf8_lossy(&msg));
+        send.write(msg.as_slice()).await?;
     }
+
+    Ok(())
+}
+
+async fn recv_packet(tx: Sender<Vec<u8>>, recv: &mut RecvStream) -> Result<()> {
+    let mut buf = vec![0; u16::MAX as usize];
+
+    let size = recv.read(&mut buf).await;
+
+    let size = match size {
+        Err(err) => panic!("{}", err),
+        Ok(n) => match n {
+            None => return Ok(()),
+            Some(m) => m,
+        },
+    };
+
+    buf.resize(size, 0);
+
+    println!("[ RECV ] {}", String::from_utf8_lossy(&buf));
+
+    tx.send(buf).unwrap();
 
     Ok(())
 }
